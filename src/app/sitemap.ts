717517eafccf434669c4098d01757b0f8ddec355
Jayesh807 <jayeshmalviya051@gmail.com>
2026-07-12 23:03:35 +0530

    add

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -15,6 +15,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: "https://studysparks.cloud/features",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://studysparks.cloud/contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
